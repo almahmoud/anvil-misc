336e518456efed3f5dc7e6a98fec45280955df77
almahmoud <amahmou4@jhu.edu>
2021-03-23 00:51:28 -0400

    Recombine

--- .github/scripts/report_deployment.sh
+++ .github/scripts/report_deployment.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+set -xe
 
-OUT=$(cat $1)
-DURATION=$(echo $OUT | grep "real" | grep -o "[0-9][a-z0-9\.]\+")
-STATUS=$(echo $OUT | grep -oP '(?<=STATUS\: )\w+')
-DATE=$(echo $OUT | grep -oP '(?<=LAST DEPLOYED\: )[^\n]+')
+DURATION=$(cat $1 | grep "real" | grep -o "[0-9][a-z0-9\.]\+")
+STATUS=$(cat $1 | grep "STATUS:" | grep -oP '(?<=STATUS\: )\w+')
+DATE=$(cat $1 | grep "LAST DEPLOYED:" | grep -oP '(?<=LAST DEPLOYED\: )[^\n]+')
 GIT_TOKEN=$2
+GIT_BRANCH=master
 REMOTE="https://$GITHUB_ACTOR:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
 setup_git() {
   # initializes git info
   git config --local user.email "action@github.com"
   git config --local user.name "GitHub Action"
+  git pull
 }
 
 push_report() {
   git add .
   git commit -m "Submitting report for deployment from $DATE"
-  git push "$CHART_REMOTE" "HEAD:$GIT_BRANCH" -v -v
+  git push "$REMOTE" "HEAD:$GIT_BRANCH" -v -v
 }
 
 setup_git
-python .github/scripts/report_deployment.py anvil '{"status": "$STATUS", "time": "$DURATION", "date": "$DATE"}'
+python .github/scripts/report_deployment.py anvil "{\"status\": \"$STATUS\", \"time\": \"$DURATION\", \"date\": \"$DATE\"}"
 push_report
