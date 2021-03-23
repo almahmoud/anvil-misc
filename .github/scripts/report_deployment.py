f77cb8a8d416423e4e0463f232478ccb19a4434a
almahmoud <amahmou4@jhu.edu>
2021-03-23 03:02:13 -0400

    Add basic HTML

--- .github/scripts/report_deployment.py
+++ .github/scripts/report_deployment.py
@@ -15,4 +15,10 @@ with open("reports/{}/deployments.json".format(project), "r+") as f:
     f.write(json.dumps(out, indent=4))
     f.truncate()
      
+with open("reports/{}/deployments.html".format(project), "r+") as f:
+    htmlout = "<table><thead><tr><th>Date</th><th>Status</th><th>GKM time</th></tr></thead><tbody>"
+    for each in deps:
+        htmlout += "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(each.get("date"), each.get("status"), each.get("time"))
+    htmlout += "</tbody></table>"
+    f.write(htmlout)
 
