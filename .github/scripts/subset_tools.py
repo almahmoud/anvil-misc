66e5cfaaa15e7a65b37f8fd73c180267beb5c283
Alexandru Mahmoud <amahmou4@jhu.edu>
2021-03-25 13:57:52 -0400

    Change to twice a day run

--- .github/scripts/subset_tools.py
+++ .github/scripts/subset_tools.py
@@ -4,15 +4,21 @@ import datetime
 
 args = sys.argv
 outfile = args[1]
-weekday = datetime.datetime.today().weekday()
+today = datetime.datetime.today()
+weekday = today.weekday()
+time_of_day = 0 if today.strftime("%p") == 'AM' else 1
+runs_per_day = 2 # run twice a day
+total_days = 7 * runs_per_day
+
 
 with open("reports/anvil/tools.yaml", 'r') as f:
     out = yaml.safe_load(f.read())
 
 l = out.get('tools', [])
-num = int(len(l)/7)
-start = weekday*num
-end = num + weekday * num if weekday < 6 else len(l)
+num_per_run = int(len(l)/total_days)
+start = (weekday * runs_per_day + time_of_day) * num_per_run
+end = (weekday * runs_per_day + time_of_day + 1) * num_per_run
+end = len(l) if end > len(l)
 out['tools'] = l[start:end]
 
 
