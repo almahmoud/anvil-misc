0e7665e5368203574ce57ce17d39785be6382397
almahmoud <amahmou4@jhu.edu>
2021-03-25 23:01:36 -0400

    Using user key instead of master key

---
+++ .github/scripts/create_api_key.py
@@ -0,0 +1,17 @@
+import sys
+
+import bioblend
+
+args = sys.argv
+
+address = args[1]
+master_key = args[2]
+
+gi = galaxy.GalaxyInstance(url=address, key=master_key)
+users = UserClient(gi)
+u = users.get_users()[0]
+key = users.create_user_apikey(u.get('id'))
+
+print(key)
+
+
