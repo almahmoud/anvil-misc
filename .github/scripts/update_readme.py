25a0578f4c7e154d520749d143d1353d87f9d51d
almahmoud <amahmou4@jhu.edu>
2021-04-06 22:01:26 -0400

    Fix readme

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -25,10 +25,9 @@ with open(allchunks, 'r+') as f:
 with open(".github/templates/README.md.j2", "r") as f:
     template = Template(f.read())
 
-with open("README.md".format(project), "w") as f:
+with open("README.md", "w") as f:
     htmlout = "<thead><tr><th>Chunk ID</th><th>Tool List</th><th>Latest report</th><th>Date of latest</th><th>Previous report</th><th>Date of previous</th></tr></thead><tbody>"
     for eachid, eachchunk in chunks:
         htmlout += "<tr><td>{}</td><td>[Toolset]({})</td><td>[Latest report]({})</td><td>{}</td><td>[Previous report]({})</td><td>{}</td></tr>".format(eachid, eachchunk.get("tools"), eachchunk.get("run1"), eachchunk.get("date1"), eachchunk.get("run2"), eachchunk.get("date2"))
     htmlout += "</tbody>"
     f.write(template.render(anviltools=htmlout))
-
